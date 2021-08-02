f7c9ef264249f468203374601d3bb2279e0f2714
jeremyakd <jeremiasc@exo.com.ar>
2021-08-02 20:49:17 -0300

    Se cambiaron las vistas para usar render y templates html. Se creó template base para haredar de ella en el resto de las templates. Se agreraron archivos estáticos.

--- web_personal/core/views.py
+++ web_personal/core/views.py
@@ -1,29 +1,13 @@
 from django.shortcuts import render, HttpResponse
 
-base_html = """
-    <h1>Mi primera vista de Django</h1>
-    <ul>
-        <li><a href="/">Home</li>
-        <li><a href="/about">Acerca de</li>
-        <li><a href="/contact">Contacto</li>
-        <li><a href="/portfolio">Portafolio</li>
-    </ul>
-"""
-
 def home(request):
-    content = ''
-    for i in range(10):
-        content += '<p> Yo soy un parrafo de for en el ciclo: ' + str(i+1) + '<p>'
-    return HttpResponse((base_html + content))
-
-# Request => Solicitud
-# Response => Respuesta
+    return render(request, 'core/home.html')
 
 def about(request):
-    return HttpResponse(base_html +  '<h1>Acerca de</h1>')
+    return render(request, 'core/about.html')
 
 def portfolio(request):
-    return HttpResponse(base_html + '<h1>Portafolio</h1>')
+    return render(request, 'core/portfolio.html')
 
 def contact(request):
-    return HttpResponse(base_html + '<h1>Contacto</h1>')
+    return render(request, 'core/contact.html')
